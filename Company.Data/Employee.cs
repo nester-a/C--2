e576fda482bc65390544711d449139b6eca101c2
Aleksey <nester.a.l@yandex.ru>
2021-03-24 22:51:18 +0300

    24.03.2021
    сделал ДЗ 1 и 2
    пункт 3 и 4 в процессе (как раз делаю контрол для редактирования сотрудника

---
+++ Company.Data/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Department Department { get; set; }
+        //public string DepartmentString
+        //{
+        //    get
+        //    {
+        //        if (this.Department == Department.Sales)
+        //            return "Sales";
+        //        else if (this.Department == Department.Purchasing)
+        //            return "Purchasing";
+        //        else
+        //            return "Service";
+        //    }
+        //}
+
+        public string Comment { get; set; }
+        public string NameAndSurname { get => $"{Name} {Surname}"; }
+
+        public Employee()
+        {
+
+        }
+        public Employee(string name, string surname, Department department)
+        {
+            Name = name;
+            Surname = surname;
+            Department = department;
+        }
+    }
+}
